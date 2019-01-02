7381c6c92ffe991272578051dae013c47b3bfce0
Martín M <skydiver@users.noreply.github.com>
2019-01-02 18:06:15 -0300

    updated project files

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-/*
- * tower-alfred-workflow
- * Copyright(c) 2015 Nicholas Penree <nick@penree.com>
- * MIT Licensed
- */
-
 const alfredo = require('alfredo');
-const exists = require('fs').existsSync;
 const tower = require('./tower');
-
-
-
-
-
-
 
 const args = process.argv;
 const items = tower.getItems(args.length >= 3 ? args[2] : null);
