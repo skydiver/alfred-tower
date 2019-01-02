1135d1e90bbb0251daef2872d5ccc6b1d910dd49
Martín M <skydiver@users.noreply.github.com>
2019-01-02 19:06:13 -0300

    added extra packages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const alfredo = require('alfredo');
+const alfredNotifier = require('alfred-notifier');
 const tower = require('./tower');
+
+alfredNotifier();
 
 const args = process.argv;
 const items = tower.getItems(args.length >= 3 ? args[2] : null);
